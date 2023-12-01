2688afd677668f708b7160535f89e223788ec230
Tomo Suzuki <tomohito95@hotmail.com>
2023-12-02 02:47:32 +0800

    Use isdigit() for part 1

--- 01/main1.py
+++ 01/main1.py
@@ -1,11 +1,10 @@
 # Part 1
-digit_set = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 total = 0
 
 with open("input.txt") as file:
     for line in file:
         line = line.strip()
-        int_only = [character for character in line if character in digit_set]
+        int_only = [character for character in line if character.isdigit()]
         total += int(f"{int_only[0]}{int_only[-1]}")
 
 print(total)
